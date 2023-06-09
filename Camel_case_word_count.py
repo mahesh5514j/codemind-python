a139024203dfb32669244eeb9095e9d494f94bb3
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-09 10:29:40 +0530

    n=input() c=1 for i in range(1,len(n)-1):     if n[i].isupper():         c=c+1 print(c)

--- Camel_case_word_count.py
+++ Camel_case_word_count.py
@@ -1,8 +1,6 @@
-def c(n):
-    c=1
-    for i in range(1,len(n)-1):
-        if n[i].isupper():
-            c=c+1
-    return(c)
 n=input()
-print(c(n))
+c=1
+for i in range(1,len(n)-1):
+    if n[i].isupper():
+        c=c+1
+print(c)

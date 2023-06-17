deffb6f3b6fe5e284dcb1b3771a6bc241e5c1cd1
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-17 23:39:16 +0530

    n=int(input()) a=list(map(int,input().split())) x=[] for i in range(0,n,2):     for j in range(a[i]):         x.append(a[i+1]) for i in x:     print(i,end=' ')

--- Decompress_Run-Length_Encoded_List.py
+++ Decompress_Run-Length_Encoded_List.py
@@ -1,5 +1,8 @@
 n=int(input())
 a=list(map(int,input().split()))
+x=[]
 for i in range(0,n,2):
     for j in range(a[i]):
-        print(a[i+1],end=' ')
+        x.append(a[i+1])
+for i in x:
+    print(i,end=' ')

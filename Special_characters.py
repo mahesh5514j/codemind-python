bbd65805593d03aaf2117ee442724c85d7cc716e
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-18 00:41:09 +0530

    n=input().lower() x=[] y=[] m='1234567890' o='qwertyuioplkjhgfdsazxcvbnm' c=0 for i in n:     if i in m:         if int(i)%2==0:             x.append(i)         else:             y.append(i)     if i not in m and i not in o:         c=c+1 if c%2==0:     for k in range((max(len(x),len(y)))):         if k<len(x):             print(x[k],end='')         if k<len(y):             print(y[k],end='') else:     for k in range((max(len(x),len(y)))):         if k<len(y):             print(y[k],end='')         if k<len(x):             print(x[k],end='')

---
+++ Special_characters.py
@@ -0,0 +1,27 @@
+n=input().lower()
+x=[]
+y=[]
+m='1234567890'
+o='qwertyuioplkjhgfdsazxcvbnm'
+c=0
+for i in n:
+    if i in m:
+        if int(i)%2==0:
+            x.append(i)
+        else:
+            y.append(i)
+    if i not in m and i not in o:
+        c=c+1
+if c%2==0:
+    for k in range((max(len(x),len(y)))):
+        if k<len(x):
+            print(x[k],end='')
+        if k<len(y):
+            print(y[k],end='')
+else:
+    for k in range((max(len(x),len(y)))):
+        if k<len(y):
+            print(y[k],end='')
+        if k<len(x):
+            print(x[k],end='')
+        

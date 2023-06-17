b65555e8c89949f3479a59ffc757dae438b56e74
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-17 23:44:22 +0530

    n=int(input()) a=list(map(int,input().split())) m=int(input()) x=[] y=[] for j in range(n):     if j>=m:         y.append(a[j])     else:         x.append(a[j]) for i in range(len(x)):     print(x[i],end=' ')     print(y[i],end=' ')

--- _Shuffle_the_Array.py
+++ _Shuffle_the_Array.py
@@ -3,11 +3,11 @@ a=list(map(int,input().split()))
 m=int(input())
 x=[]
 y=[]
-for i in range(m):
-    x.append(a[i])
 for j in range(n):
     if j>=m:
         y.append(a[j])
+    else:
+        x.append(a[j])
 for i in range(len(x)):
     print(x[i],end=' ')
     print(y[i],end=' ')

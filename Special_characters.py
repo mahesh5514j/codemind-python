e7eee9013107a329ade43bd6a5a018e3398903c9
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-18 00:41:49 +0530

    n=input().lower() x=[] y=[] m='1234567890' z='qwertyuioplkjhgfdsazxcvbnm' c=0 for i in n:     if i in m:         if int(i)%2==0:             x.append(i)         else:             y.append(i)     if i not in m and i not in z:         c=c+1 if c%2==0:     for k in range((max(len(x),len(y)))):         if k<len(x):             print(x[k],end='')         if k<len(y):             print(y[k],end='') else:     for k in range((max(len(x),len(y)))):         if k<len(y):             print(y[k],end='')         if k<len(x):             print(x[k],end='')

--- Special_characters.py
+++ Special_characters.py
@@ -2,7 +2,7 @@ n=input().lower()
 x=[]
 y=[]
 m='1234567890'
-o='qwertyuioplkjhgfdsazxcvbnm'
+z='qwertyuioplkjhgfdsazxcvbnm'
 c=0
 for i in n:
     if i in m:
@@ -10,7 +10,7 @@ for i in n:
             x.append(i)
         else:
             y.append(i)
-    if i not in m and i not in o:
+    if i not in m and i not in z:
         c=c+1
 if c%2==0:
     for k in range((max(len(x),len(y)))):
@@ -24,4 +24,3 @@ else:
             print(y[k],end='')
         if k<len(x):
             print(x[k],end='')
-        

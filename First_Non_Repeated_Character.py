d41b09077008bc4e88007440081a3be7506c50fd
mahesh <99953665+mahesh5514j@users.noreply.github.com>
2023-06-18 11:50:41 +0530

    for i in range(int(input())):     n=int(input())     a=input()     for i in a:         if a.count(i)==1 and i!='':             print(i)             break     else:         print("-1")

--- First_Non_Repeated_Character.py
+++ First_Non_Repeated_Character.py
@@ -1,12 +1,9 @@
 for i in range(int(input())):
     n=int(input())
     a=input()
-    x=[]
     for i in a:
         if a.count(i)==1 and i!='':
-            x.append(i)
+            print(i)
             break
-    if len(x)==1:
-        print(x[0])
     else:
         print("-1")
